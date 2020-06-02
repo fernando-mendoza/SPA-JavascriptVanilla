469b251334db2b525a1cfee9ef247b13222dab0e
fernando-mendoza <55415857+fernando-mendoza@users.noreply.github.com>
2020-06-01 21:37:05 -0500

    webpack configs
    webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+  plugins: [new HTMLWebpackPlugin()],
+};
